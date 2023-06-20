64d7b958588e35277e01c482a37ef037d2b2bf38
SibabalweMvunelwa <sibabalwemvunelwa@gmail.com>
2023-06-21 00:39:29 +0200

    DWA6 - Work in progress

--- scripts.js
+++ scripts.js
@@ -241,3 +241,4 @@ document.querySelector('[data-list-items]').addEventListener('click', (event) =>
         document.querySelector('[data-list-description]').innerText = active.description
     }
 })
+
